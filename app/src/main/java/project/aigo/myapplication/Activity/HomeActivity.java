7ec1971860323aa2d0caf3deb067259771bd3053
andreasNicholas <andreas.nicholas@yahoo.com>
2018-11-23 19:53:29 +0700

    Push ulang

--- app/src/main/java/project/aigo/myapplication/Activity/HomeActivity.java
+++ app/src/main/java/project/aigo/myapplication/Activity/HomeActivity.java
@@ -8,10 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import project.aigo.myapplication.Adapter.ImageSliderAdapter;
+import project.aigo.myapplication.Object.Donation;
+import project.aigo.myapplication.Object.News;
 import project.aigo.myapplication.R;
 
 public class HomeActivity extends AppCompatActivity {
+    public static TextView tvDonationTitle;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,6 +43,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //chnage later get from DB
+        Donation donation = new Donation("this is donation 1", R.drawable.ic_launcher_background, 0, "content for donation 1");
+        donation.donationList.add(donation);
+        Donation donation2 = new Donation("this is donation 2", R.drawable.ic_launcher_background, 0, "content for donation");
+        donation2.donationList.add(donation2);
+        Donation donation3 = new Donation("this is donation 3", R.drawable.ic_launcher_foreground, 0, "content for donation 3");
+        donation3.donationList.add(donation3);
+        News news = new News("this is news 1", R.drawable.ic_launcher_background, 0, "content for news 1");
+        news.newsList.add(news);
+        News news2 = new News("this is news 2", R.drawable.ic_launcher_background, 0, "content for news 2");
+        news.newsList.add(news2);
+        News news3 = new News("this is news 3", R.drawable.ic_launcher_foreground, 0, "content for news 3");
+        news.newsList.add(news3);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
--- app/src/main/java/project/aigo/myapplication/Adapter/ImageSliderAdapter.java
+++ app/src/main/java/project/aigo/myapplication/Adapter/ImageSliderAdapter.java
@@ -3,23 +3,42 @@ package project.aigo.myapplication.Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import project.aigo.myapplication.Activity.HomeActivity;
+import project.aigo.myapplication.Activity.LoginActivity;
+import project.aigo.myapplication.Object.Donation;
+import project.aigo.myapplication.Object.News;
 import project.aigo.myapplication.R;
 
 public class ImageSliderAdapter extends PagerAdapter {
     private Context mContext;
-    private int[] mImageIds = new int[]{R.drawable.ic_search_white_24dp, R.drawable.ic_dashboard_black_24dp, R.drawable.ic_launcher_background};
+    private int[] mImageIds;
+    private String[] mImageTitles;
+    LayoutInflater layoutInflater;
 
     public ImageSliderAdapter(Context context){
         mContext = context;
+        mImageIds = new int[Donation.donationList.size()];
+        for(int i=0; i<mImageIds.length; i++){
+            mImageIds[i] = Donation.donationList.get(i).getImage_id();
+        }
+        mImageTitles = new String[Donation.donationList.size()];
+        for(int i=0; i<mImageTitles.length; i++){
+            mImageTitles[i] = Donation.donationList.get(i).getDonation_title();
+        }
     }
 
     @Override
     public int getCount() {
-        return mImageIds.length;
+        return mImageTitles.length;
     }
 
     @Override
@@ -30,15 +49,23 @@ public class ImageSliderAdapter extends PagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        ImageView imageView = new ImageView(mContext);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setImageResource(mImageIds[position]);
-        container.addView(imageView, 0);
-        return imageView;
+        layoutInflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide, container, false);
+
+        ImageView imageSlide = view.findViewById(R.id.ivSlide);
+        TextView titleSlide = view.findViewById(R.id.tvSlide);
+
+        imageSlide.setImageResource(mImageIds[position]);
+        imageSlide.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageSlide.getLayoutParams().height = ((mContext.getResources().getDisplayMetrics().heightPixels)/4);
+        titleSlide.setText(mImageTitles[position]);
+
+        container.addView(view);
+        return view;
     }
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView((ImageView)object);
+        container.removeView((LinearLayout)object);
     }
 }
